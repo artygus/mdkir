85eadfff6c047ff27fd59038e4d893e8e77c0465
artygus <hero@engineeriam.net>
2024-08-09 13:58:59 +0100

    checksum formula

--- mdkir.rb
+++ mdkir.rb
@@ -1,4 +1,7 @@
 class Mdkir < Formula
+  version "0.1"
+  sha256 "2273a16f3ba6977ac1798a3082e2654040ec46c4b84a32d136ba4ba69a3adc86"
+  url "https://github.com/artygus/mdkir/archive/refs/tags/v#{version}.tar.gz"
   desc "Prints Kir if you type mdkir instead of mkdir"
   license "MIT"
   head "https://github.com/artygus/mdkir.git", branch: "main"
